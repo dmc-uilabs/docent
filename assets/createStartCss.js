8fd696806a529dd52bbbe1e34921b178cdf0935a
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-06-07 11:00:30 -0500

    sa and dmc working with remote loads, fonts working, but possible bad version

--- assets/createStartCss.js
+++ assets/createStartCss.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 var inData = fs.readFileSync('./app/mra.css', 'utf8');
 var singleLine = inData.split('\n').join(' ');
-var encoded = encodeURI(singleLine);
-fs.writeFile('./assets/inputCss.txt', encoded, 'utf8', function(err) {
+// var encoded = encodeURI(singleLine);
+var encoded = singleLine;
+fs.writeFile('./assets/inputCss.css', encoded, 'utf8', function(err) {
   if(err) return console.log(err);
 });
